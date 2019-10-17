236489dbcf480e6cbfa4534f63ab984c58de5ad4
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-10-17 13:15:54 +0200

    for adibidea

---
+++ src/adibideak/Adibidea8.java
@@ -0,0 +1,15 @@
+package adibideak;
+
+/*
+ * for-aren adibidea
+ */
+public class Adibidea8 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++){
+			System.out.println(i);
+		}
+	}
+
+}
