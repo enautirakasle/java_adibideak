8dfed0d7e59528bcf6bd3641e8e88b71833d6dab
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-10-18 10:28:39 +0200

    string adibideak

---
+++ src/adibideak/Adibidea10.java
@@ -0,0 +1,34 @@
+package adibideak;
+/*
+ * stringak ikasten
+ */
+public class Adibidea10 {
+
+	public static void main(String[] args) {
+		String aldagaiIzena; //definitu
+		aldagaiIzena = "balioa"; //asignazioa
+		
+		int adina = 25;
+		
+		String herria = "Zornotza"; //definizioa eta asignazioa
+		
+		String kateBatua = herria + " da nire ikastetxearen herria da"; //konkatenazioa
+		
+//		System.out.println(kateBatua);
+//		
+//		System.out.println(herria + "koa naiz eta " + adina + " urte ditut.");
+		
+//		char letra = herria.charAt(herria.length()-1);
+//		
+//		System.out.println("Ondarroa".charAt(1));
+		
+		for(int i = 0; i < herria.length(); i++){
+			System.out.println(herria.charAt(i));
+		}
+		
+		
+		
+
+	}
+
+}
