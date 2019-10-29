23acaca8d0ddab2d353865f7111ddf56156396ef
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-10-29 13:00:11 +0100

    fitxategia irakurri eta arrayan gorde

---
+++ src/adibideak/Adibidea14.java
@@ -0,0 +1,38 @@
+package adibideak;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+ * datuak fitxategitik irakurri eta pantailan idatzi
+ */
+public class Adibidea14 {
+
+	public static void main(String[] args) {
+		//fitxategia zabaldu
+		File fitxategia = new File("C:/Users/ikaslea/Desktop/datuak.txt");
+		Scanner scan;
+		String[] lerroak = new String[100];
+		
+		try {
+			scan = new Scanner(fitxategia);
+			String lerroa;
+			int i = 0;
+			while(scan.hasNext()){//lerroren bat dagoen bitaretan
+				//irakukrri lerroa
+				lerroa = scan.nextLine();
+				//idatzi lerroa
+				System.out.println(lerroa);
+				lerroak[i] = lerroa;
+				i++;
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+
+	}//main bukaera
+
+}
