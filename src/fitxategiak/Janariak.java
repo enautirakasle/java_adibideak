8784d5dff5edf65af12cb44a6150cfd5eafb4faa
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-11-07 14:09:32 +0100

    fruitu lehorrak zerrendatu eginda

--- src/fitxategiak/Janariak.java
+++ src/fitxategiak/Janariak.java
@@ -9,7 +9,7 @@ public class Janariak {
 	public static void main(String[] args) {
 		String lerroa;
 		String[] zatiak;
-		
+
 		String[] izenak = new String[404];
 		String[] egoerak = new String[404];
 		double[] kaloriak = new double[404];
@@ -18,51 +18,80 @@ public class Janariak {
 		double[] karbohidratoak = new double[404];
 		String[] motak = new String[404];
 
-		
-		//datuen karga
+		// datuen karga
 		File file = new File("datuak/janariak.txt");
 		try {
 			Scanner scan = new Scanner(file);
-			
+
 			int i = 0;
-			while(scan.hasNext()){
+			while (scan.hasNext()) {
 				lerroa = scan.nextLine();
 				zatiak = lerroa.split(";");
-				
+
 				izenak[i] = zatiak[0];
 				egoerak[i] = zatiak[1];
 				kaloriak[i] = Double.parseDouble(zatiak[2].replace(',', '.'));
-				
+
 				String proteinaKomarekin = zatiak[3];
 				String proteinaPuntuarekin = zatiak[3].replace(',', '.');
 				proteinak[i] = Double.parseDouble(proteinaPuntuarekin);
-				
-				
+
 				gantzak[i] = Double.parseDouble(zatiak[4].replace(',', '.'));
 				karbohidratoak[i] = Double.parseDouble(zatiak[5].replace(',', '.'));
 				motak[i] = zatiak[6];
-				
+
 				i++;
 			}
-	
+			scan.close();
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		//datuen karga bukatu
-		
-		
-		//menua eta aukerak
-		
-		
-		
-		//datuak gorde
-		
-		
-		
-		
+		// datuen karga bukatu
 
-	
+		// menua eta aukerak
+		Scanner scanTeklatua = new Scanner(System.in);
+		int aukera;
+		do {
+			// menua erakutsi
+			System.out.println("----MENUA----\n0-irten\n1- fruitu lehorrak zerrendatu");
 
-	}
+			// aukera irakurri
+			aukera = Integer.parseInt(scanTeklatua.nextLine());
 
-}
+			// switch batekin aukera bakoitza inplementatu
+			switch (aukera) {
+			case 1:
+				fruituLehorrakZerrendatu(izenak, egoerak, kaloriak, gantzak, proteinak,karbohidratoak, motak);
+				break;
+			case 0:
+
+				System.out.println("Programa zarratzera doa...");
+				break;
+			default:
+				System.out.println("Aukera okerra");
+				break;
+			}
+
+		} while (aukera != 0);
+
+		//TODO datuak gorde
+
+	}//main bukaera
+
+	static void fruituLehorrakZerrendatu(String[] izenak, String[] egoerak, double[] kaloriak, double[] gantzak,
+			double[] proteinak, double[] karbohidratoak, String[] motak) {
+
+		for(int i = 0; i<izenak.length;i++){
+			
+			if(motak[i].equals("fruto seco")){
+				System.out.println("Janari izena: " + izenak[i] 
+									+ "\n\tEgoera: " + egoerak[i] 
+									+ "\n\tKaloriak: " 	+ kaloriak[i]);
+			}
+			
+		}
+		
+	}//fruituLehorrakZerrendatu bukaera
+
+}//class bukaera
