75440868f42431e03b32cc00c06aa5b44742af83
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-11-26 11:04:25 +0100

    aurkularren ariketaren algoritmoa

---
+++ src/opp/aurikularrak/Main.java
@@ -0,0 +1,18 @@
+package opp.aurikularrak;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//debinitu aurikularrez osatutako 10eko array bat
+		
+		//fitxategia irakurri
+		//lerro bakoitzeko
+			//aurikular bat sortu
+			//objetu hori bete datuak bete
+			//objetua arrayra sartu
+		
+		//arraya pantailaratu
+
+	}
+
+}
