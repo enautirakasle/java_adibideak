5681c12df0f82d84c68a7a425486f4dd918dfd92
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-12-02 09:07:06 +0100

    string eta int zerrendak

--- src/listaDinamikoak/Main.java
+++ src/listaDinamikoak/Main.java
@@ -7,28 +7,54 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<String> izenak = new ArrayList<String>();
-		
+
 		izenak.add("Markel");
 		izenak.add("Igor");
 		izenak.add("Iker");
-		
-		//izenak.remove(2);
-		
-		pantailaratu1(izenak);
-		pantailaratu2(izenak);
+
+		// izenak.remove(2);
+
+		pantailaratuSt1(izenak);
+		pantailaratuSt2(izenak);
+
+		ArrayList<Integer> zenbakiak = new ArrayList<Integer>();
+		zenbakiak.add(23);
+		zenbakiak.add(11);
+		zenbakiak.add(67);
+
+		pantailaratuZenb1(zenbakiak);
+		pantailaratuZenb2(zenbakiak);
 	}
 
-	private static void pantailaratu2(ArrayList<String> stringLista) {
+	private static void pantailaratuZenb2(ArrayList<Integer> zenbakiak) {
+		Iterator<Integer> i = zenbakiak.iterator();
+
+		while (i.hasNext()) {
+			int zenbakia = i.next();
+			System.out.println(zenbakia);
+		}
+
+	}
+
+	private static void pantailaratuZenb1(ArrayList<Integer> zenbakiak) {
+		for (int i = 0; i < zenbakiak.size(); i++) {
+			int zenbakia = zenbakiak.get(i);
+			System.out.println(zenbakia);
+		}
+
+	}
+
+	private static void pantailaratuSt2(ArrayList<String> stringLista) {
 		Iterator<String> i = stringLista.iterator();
-		
-		while(i.hasNext()){
+
+		while (i.hasNext()) {
 			String katea = i.next();
 			System.out.println(katea);
 		}
 	}
 
-	private static void pantailaratu1(ArrayList<String> stringLista) {
-		for(int i = 0; i<stringLista.size(); i++){
+	private static void pantailaratuSt1(ArrayList<String> stringLista) {
+		for (int i = 0; i < stringLista.size(); i++) {
 			String katea = stringLista.get(i);
 			System.out.println(katea);
 		}
