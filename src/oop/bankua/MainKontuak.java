98a54f168d71d381d31c48f0d3e4c3a7b2393308
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-12-05 14:40:17 +0100

    kontu kudeatzaile hasiera

---
+++ src/oop/bankua/MainKontuak.java
@@ -0,0 +1,41 @@
+package oop.bankua;
+
+import java.util.ArrayList;
+
+public class MainKontuak {
+	
+	static final int IRTEN = 0;
+	static final int KONTUA_SORTU = 1;
+	static final int KONTUA_EZABATU = 2;
+	
+	static final int KONTUA_IKUSI = 3;
+	static final int INGRESOA_EGIN = 4;
+	static final int DIRUA_ATERA = 5;
+	
+	
+	public static void main(String[] args) {
+		ArrayList<KontuKorrontea> kontuKorronteak = new ArrayList<KontuKorrontea>();
+		
+		//kontu konrronte bat sortu
+		
+		//balio literalekin bete
+		
+		//kontua listara gehitu
+		
+		
+		
+		//do while
+		//	menua
+		//	aukera irakurri
+		//	swicha
+		
+		
+		
+		
+	}
+	
+	static void menuNagusia(){
+		System.out.println("--Menua--\n"
+				+ IRTEN + " - irten");
+	}
+}
