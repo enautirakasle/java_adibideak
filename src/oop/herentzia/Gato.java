b2cb3ee5f8331661858d116a36a630735447bdf2
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-12-12 14:23:19 +0100

    lehen commita

---
+++ src/oop/herentzia/Gato.java
@@ -0,0 +1,8 @@
+package oop.herentzia;
+
+public class Gato extends Animal {
+
+	public void maullar(){
+		System.out.println("Miau");
+	}
+}
---
+++ src/oop/herentzia/Loro.java
@@ -0,0 +1,9 @@
+package oop.herentzia;
+
+public class Loro extends Animal{
+	
+	public void hablar(String frase){
+		System.out.println(frase);
+	}
+
+}
