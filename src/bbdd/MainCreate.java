78845122cce53a0951ae87b216b2f81fcc8756ab
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-01-17 09:00:26 +0100

    create usuario clasea erabilita

--- src/bbdd/MainCreate.java
+++ src/bbdd/MainCreate.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class MainCreate {
 	private static final String HOST = "localhost";
@@ -15,21 +16,21 @@ public class MainCreate {
 	private static final String PASSWORD = "";
 
 	public static void main(String[] args) {
-		String nombre, apellido, dni, password;
-		int id;
-		boolean esAdmin;
+	
 
 		Connection conexion;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conexion = DriverManager.getConnection("jdbc:mysql://" + HOST + "/" + BBDD, USERNAME, PASSWORD);
 			
+			Usuario usuario = leerUsuario();
+			
 			PreparedStatement pstInsert = conexion.prepareStatement("INSERT INTO usuarios(nombre, apellido, dni, admin, password) VALUES (?, ? ,?, ?, ?)");
-			pstInsert.setString(1, "aitor");
-			pstInsert.setString(2, "badiola");
-			pstInsert.setString(3, "11223344w");
-			pstInsert.setBoolean(4, false);
-			pstInsert.setString(5, "badiola2020");
+			pstInsert.setString(1, usuario.getNombre());
+			pstInsert.setString(2, usuario.getApellido());
+			pstInsert.setString(3, usuario.getDni());
+			pstInsert.setBoolean(4, usuario.isAdmin());
+			pstInsert.setString(5, usuario.getPassword());
 			pstInsert.execute();
 
 			conexion.close();
@@ -40,5 +41,26 @@ public class MainCreate {
 			e.printStackTrace();
 		}
 
+	}
+
+	private static Usuario leerUsuario() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Escribe el nombre");
+		String nombre = scan.nextLine();
+		System.out.println("Escribe el apellid");
+		String apellido = scan.nextLine();
+		System.out.println("Escribe el dni");
+		String dni = scan.nextLine();
+		System.out.println("Escribe el password");
+		String password = scan.nextLine();
+		
+		Usuario usuario = new Usuario();
+		usuario.setNombre(nombre);
+		usuario.setApellido(apellido);
+		usuario.setDni(dni);
+		usuario.setPassword(password);
+		usuario.setAdmin(false);
+		
+		return usuario;
 	}
 }
