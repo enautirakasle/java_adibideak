6be73f9e86f02357c8a684f0386a99066844fa49
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-01-20 08:41:36 +0100

    conexio clasea

---
+++ src/bbdd/liburutegia/libro/Conexion.java
@@ -0,0 +1,10 @@
+package bbdd.liburutegia.libro;
+
+public abstract class Conexion {
+	protected Conexion conexion;
+	
+	Conexion(){
+		//TODO crear la conexion
+		//guardar la conexion en this.conexion
+	}
+}
---
+++ src/bbdd/liburutegia/usuario/Conexion.java
@@ -0,0 +1,10 @@
+package bbdd.liburutegia.usuario;
+
+public abstract class Conexion {
+	protected Conexion conexion;
+	
+	Conexion(){
+		//TODO crear la conexion
+		//guardar la conexion en this.conexion
+	}
+}
