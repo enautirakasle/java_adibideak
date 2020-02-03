e0b560c2a440f48618c6f026aab296820507d760
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-02-03 09:56:52 +0100

    prestamos de libro eginda

--- src/bbdd/liburutegia/prestamo/ModeloPrestamo.java
+++ src/bbdd/liburutegia/prestamo/ModeloPrestamo.java
@@ -60,6 +60,54 @@ public class ModeloPrestamo extends Conexion{
 		return prestamos;
 	}
 	
+	public ArrayList<Prestamo> prestamosDeLibro(int idLibro){
+		ArrayList<Prestamo> prestamos = new ArrayList<Prestamo>();
+		
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement("SELECT prestamos.*, "
+					+ "libros.titulo, libros.autor, libros.num_pag, "
+					+ "socios.nombre, socios.apellido, socios.dni, socios.direccion, socios.poblacion, socios.provincia "
+					+ "FROM (prestamos join libros on prestamos.id_libro=libros.id) "
+					+ "join socios on prestamos.id_socio=socios.id "
+					+ "where libros.id= ?");
+			pst.setInt(1, idLibro);
+			
+			ResultSet rs = pst.executeQuery();
+			
+			while(rs.next()){
+				Prestamo prestamo = new Prestamo();
+				prestamo.setFecha(rs.getDate("fecha"));
+				prestamo.setDevuelto(rs.getBoolean("devuelto"));
+				
+				Libro libro = new Libro();
+				libro.setId(rs.getInt("id_libro"));
+				libro.setTitulo(rs.getString("titulo"));
+				libro.setAutor(rs.getString("autor"));
+				libro.setNumeroPaginas(rs.getInt("num_pag"));
+				prestamo.setLibro(libro);
+				
+				Socio socio = new Socio();
+				socio.setId(rs.getInt("id_socio"));
+				socio.setNombre(rs.getString("nombre"));
+				socio.setApellido(rs.getString("apellido"));
+				socio.setDni(rs.getString("dni"));
+				socio.setDireccion(rs.getString("direccion"));
+				socio.setPoblacion(rs.getString("poblacion"));
+				socio.setProvincia(rs.getString("provincia"));
+				prestamo.setSocio(socio);
+				
+				prestamos.add(prestamo);
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return prestamos;
+	}
+	
 	public void finalizar(Prestamo prestamo){
 	
 		
