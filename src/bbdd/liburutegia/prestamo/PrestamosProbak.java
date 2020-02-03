d80d3c54cae335a453f7642532da6b359ee8c0bb
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-02-03 09:56:29 +0100

    finalizar prestamoren proba

--- src/bbdd/liburutegia/prestamo/PrestamosProbak.java
+++ src/bbdd/liburutegia/prestamo/PrestamosProbak.java
@@ -1,21 +1,40 @@
 package bbdd.liburutegia.prestamo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
+
+import bbdd.liburutegia.Libro;
+import bbdd.liburutegia.socio.Socio;
 
 public class PrestamosProbak {
 
 	public static void main(String[] args) {
-		ModeloPrestamo pm = new ModeloPrestamo("localhost", "biblioteca", "root", "");
-		ArrayList<Prestamo> prestamos= pm.selectAll();
-		
-		Iterator<Prestamo> it = prestamos.iterator();
-		while (it.hasNext()) {
-			Prestamo prestamo = (Prestamo) it.next();
-			System.out.println(prestamo);
-			
-		}
+		try {
+			ModeloPrestamo pm = new ModeloPrestamo("localhost", "biblioteca", "root", "");
 
+			Prestamo prestamo = new Prestamo();
+
+			Libro libro = new Libro();
+			libro.setId(10);
+			prestamo.setLibro(libro);
+
+			Socio socio = new Socio();
+			socio.setId(2);
+			prestamo.setSocio(socio);
+
+			// 2018-04-16
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			Date fecha = sdf.parse("2018-04-16");
+			prestamo.setFecha(fecha);
+
+			pm.finalizar(prestamo);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
